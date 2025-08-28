6c79056b344daefa5a81aa15f15f4de38e78428b
Asbjørn Olav Orvedal <asorv1536@uib.no>
2025-08-28 22:25:37 +0200

    Implement NonEmpty for proper completeness checking

--- src/Any.hs
+++ src/Any.hs
@@ -1,20 +1,30 @@
 module Any where
 
 import Data.Kind
+import GHC.TypeError
 
-data Any :: [Type] -> Type where
-  Here :: a -> Any (a ': as)
-  There :: Any as -> Any (a ': as)
+data NonEmpty :: Type where
+  (:|) :: Type -> [Type] -> NonEmpty
+
+data Any :: NonEmpty -> Type where
+  Here :: a -> Any (a ':| as)
+  There :: Any (b ':| as) -> Any (a ':| (b ': as))
 
 type family (<>) (xs :: [Type]) (ys :: [Type]) :: [Type] where
   (<>) '[] ys = ys
   (<>) (x ': xs) ys = x ': xs <> ys
 
-class Push (a :: Type) (ts :: [Type]) where
+class Push (a :: Type) (ts :: NonEmpty) where
   push :: a -> Any ts
 
-instance Push a (a ': ts) where
+instance Push a (a ':| ts) where
   push = Here
 
-instance {-# OVERLAPPABLE #-} (Push a ts) => Push a (b ': ts) where
+instance {-# INCOHERENT #-} (Push a (t ':| ts)) => Push a (b ':| (t ': ts)) where
   push = There . push
+
+type family ToNonEmpty (ts :: [Type]) :: NonEmpty where
+  ToNonEmpty (t ': ts) = t ':| ts
+  ToNonEmpty '[] = TypeError (Text "Cannot construct a NonEmpty sum type. Use a non-empty list.")
+
+type AnyOf ts = Any (ToNonEmpty ts)
